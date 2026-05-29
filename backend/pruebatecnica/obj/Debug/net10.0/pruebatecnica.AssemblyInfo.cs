54837dc589988afada043a2c76887c9b671251b4
Adonay Franco <oafa1995@hotmail.com>
2026-05-29 15:58:03 -0600

    cambiando nombre de carpetas

--- backend/pruebatecnica/obj/Debug/net10.0/pruebatecnica.AssemblyInfo.cs
+++ backend/pruebatecnica/obj/Debug/net10.0/pruebatecnica.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pruebatecnica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a37aeb04ee6e9d41eac1f3d0d7e1440312168a95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01254804269b2757f1c43f5f494a0c29f71d4e94")]
 [assembly: System.Reflection.AssemblyProductAttribute("pruebatecnica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pruebatecnica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
